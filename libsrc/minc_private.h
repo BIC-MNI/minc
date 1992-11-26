794ab40ede87c9e2e200bc41cc95336065a89018
neelin <neelin>
1992-11-26 12:34:14 +0000

    Replaced include of malloc.h with stdlib.h

--- libsrc/minc_private.h
+++ libsrc/minc_private.h
@@ -15,7 +15,7 @@
 @MODIFIED   : 
 ---------------------------------------------------------------------------- */
 
-#include  <malloc.h>
+#include  <stdlib.h>
 #include  <stdio.h>
 #include  <math.h>
 #include  <string.h>
