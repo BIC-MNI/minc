b301a19134547765bcf9e8a36876ca642e3e2358
neelin <neelin>
1993-07-12 15:18:12 +0000

    Initial revision

---
+++ progs/Proglib/minc_def.h
@@ -0,0 +1,19 @@
+/* ----------------------------- MNI Header -----------------------------------
+@NAME       : mincprogs.h
+@DESCRIPTION: Header file to define general things needed for minc programs
+@GLOBALS    : 
+@CALLS      : 
+@CREATED    : July 7, 1993
+@MODIFIED   : 
+---------------------------------------------------------------------------- */
+
+#ifndef EXIT_SUCCESS
+#  define EXIT_SUCCESS 0
+#endif
+#ifndef EXIT_FAILURE
+#  define EXIT_FAILURE 1
+#endif
+#ifndef SEEK_SET
+#  define SEEK_SET 0
+#endif
+
