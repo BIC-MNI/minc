6f5905c5feefbb4b65bf16463dc5c54f1090e1f3
neelin <neelin>
1993-07-21 12:40:30 +0000

    Added copyright notice.

--- progs/mincextract/mincextract.c
+++ progs/mincextract/mincextract.c
@@ -9,10 +9,20 @@
 @CALLS      : 
 @CREATED    : June 10, 1993 (Peter Neelin)
 @MODIFIED   : 
+@COPYRIGHT  :
+              Copyright 1993 Peter Neelin, McConnell Brain Imaging Centre, 
+              Montreal Neurological Institute, McGill University.
+              Permission to use, copy, modify, and distribute this
+              software and its documentation for any purpose and without
+              fee is hereby granted, provided that the above copyright
+              notice appear in all copies.  The author and McGill University
+              make no representations about the suitability of this
+              software for any purpose.  It is provided "as is" without
+              express or implied warranty.
 ---------------------------------------------------------------------------- */
 
 #ifndef lint
-static char rcsid[]="$Header: /private-cvsroot/minc/progs/mincextract/mincextract.c,v 1.5 1993-07-13 16:24:14 neelin Exp $";
+static char rcsid[]="$Header: /private-cvsroot/minc/progs/mincextract/mincextract.c,v 1.6 1993-07-21 12:40:30 neelin Exp $";
 #endif
 
 #include <stdlib.h>
