0041639c7c0c18e915407e0a7fc48d727fd08b73
david <david>
1993-10-06 20:19:31 +0000

    Initial revision

---
+++ volume_io/Testing/box_filter_volume.c
@@ -0,0 +1,51 @@
+#include  <mni.h>
+
+int  main(
+    int   argc,
+    char  *argv[] )
+{
+    Volume     volume, new_volume;
+    Status     status;
+    Real       x_width, y_width, z_width, separations[MAX_DIMENSIONS];
+    BOOLEAN    world_space;
+    char       *input_filename, *output_filename, *history, *dummy;
+
+    initialize_argument_processing( argc, argv );
+
+    if( !get_string_argument( "", &input_filename ) ||
+        !get_string_argument( "", &output_filename ) )
+    {
+        print( "Need args.\n" );
+        return( 1 );
+    }
+
+    (void) get_real_argument( 3.0, &x_width );
+    (void) get_real_argument( 3.0, &y_width );
+    (void) get_real_argument( 3.0, &z_width );
+
+    world_space = get_string_argument( "", &dummy );
+
+    status = input_volume( input_filename, 3, (char **) NULL,
+                      NC_UNSPECIFIED, FALSE, 0.0, 0.0,
+                      TRUE, &volume, (minc_input_options *) NULL ) ;
+
+    if( status != OK )
+        return( 1 );
+
+    if( world_space )
+    {
+        get_volume_separations( volume, separations );
+        x_width /= separations[X];
+        y_width /= separations[Y];
+        z_width /= separations[Z];
+    }
+
+    new_volume = create_box_filtered_volume( volume, x_width, y_width, z_width);
+
+    history = "box filtered";
+
+    status = output_volume( output_filename, NC_UNSPECIFIED, FALSE, 0.0, 0.0,
+                            new_volume, history, (minc_output_options *) NULL );
+
+    return( 0 );
+}
