3e778e8e4fb795e6e814284425f18b42612c472e
david <david>
1994-04-12 11:16:11 +0000

    Initial revision

---
+++ volume_io/Include/volume_io.h
@@ -0,0 +1,29 @@
+
+#ifndef  DEF_VOLUME_IO
+#define  DEF_VOLUME_IO
+
+/* ----------------------------- MNI Header -----------------------------------
+@NAME       : volume_io.h
+@INPUT      : 
+@OUTPUT     : 
+@RETURNS    : 
+@DESCRIPTION: A set of macros and definitions useful for all MNI programs.
+@METHOD     : 
+@GLOBALS    : 
+@CALLS      : 
+@CREATED    : July 15, 1991       David MacDonald
+@MODIFIED   :
+---------------------------------------------------------------------------- */
+
+#include  <basic.h>
+#include  <string_funcs.h>
+#include  <files.h>
+#include  <arrays.h>
+#include  <geom_structs.h>
+#include  <progress.h>
+#include  <geometry.h>
+#include  <volume.h>
+#include  <transforms.h>
+#include  <volume_io_prototypes.h>
+
+#endif
