fe8bc709700bbcd094e9fe57e54666bbb95903e7
swmgr <swmgr>
2002-12-11 14:34:11 +0000

    Updated generated files to match August 2002 changes to gram.c and lex.l

--- progs/minccalc/y.tab.h
+++ progs/minccalc/y.tab.h
@@ -1,56 +1,47 @@
-#ifndef BISON_Y_TAB_H
-# define BISON_Y_TAB_H
-
-#ifndef YYSTYPE
 typedef union{
 int      pos;
 node_t   node;
 double   real;
 ident_t  ident;
-} yystype;
-# define YYSTYPE yystype
-# define YYSTYPE_IS_TRIVIAL 1
-#endif
-# define	NAN	257
-# define	IN	258
-# define	TO	259
-# define	IDENT	260
-# define	REAL	261
-# define	AVG	262
-# define	PROD	263
-# define	SUM	264
-# define	LET	265
-# define	NEG	266
-# define	LEN	267
-# define	MAX	268
-# define	MIN	269
-# define	ISNAN	270
-# define	SQRT	271
-# define	ABS	272
-# define	EXP	273
-# define	LOG	274
-# define	SIN	275
-# define	COS	276
-# define	TAN	277
-# define	ASIN	278
-# define	ACOS	279
-# define	ATAN	280
-# define	CLAMP	281
-# define	SEGMENT	282
-# define	LT	283
-# define	LE	284
-# define	GT	285
-# define	GE	286
-# define	EQ	287
-# define	NE	288
-# define	NOT	289
-# define	AND	290
-# define	OR	291
-# define	IF	292
-# define	ELSE	293
-# define	FOR	294
+} YYSTYPE;
+#define	NAN	258
+#define	IN	259
+#define	TO	260
+#define	IDENT	261
+#define	REAL	262
+#define	AVG	263
+#define	PROD	264
+#define	SUM	265
+#define	LET	266
+#define	NEG	267
+#define	LEN	268
+#define	MAX	269
+#define	MIN	270
+#define	ISNAN	271
+#define	SQRT	272
+#define	ABS	273
+#define	EXP	274
+#define	LOG	275
+#define	SIN	276
+#define	COS	277
+#define	TAN	278
+#define	ASIN	279
+#define	ACOS	280
+#define	ATAN	281
+#define	CLAMP	282
+#define	SEGMENT	283
+#define	LT	284
+#define	LE	285
+#define	GT	286
+#define	GE	287
+#define	EQ	288
+#define	NE	289
+#define	NOT	290
+#define	AND	291
+#define	OR	292
+#define	IF	293
+#define	ELSE	294
+#define	FOR	295
 
 
 extern YYSTYPE yylval;
-
-#endif /* not BISON_Y_TAB_H */
