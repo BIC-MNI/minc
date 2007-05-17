55912b8e8ae11feb5e75bf8cc2906d423f51af8a
rotor <rotor>
2007-05-17 16:56:08 +0000

    * small build fixes (added libsrc2 to the INCLUDES)  * rewrote test scripts as sh. (depending on tcsh/csh is bad)

---
+++ testdir/run_tests.sh
@@ -0,0 +1,14 @@
+#! /bin/sh
+
+set -e
+
+tests=minc_types icv_range icv icv_dim icv_dim1 icv_fillvalue
+
+for test in $tests;
+do
+   echo Testing $test
+   ./$test > junk.out
+   diff $srcdir/$test.out junk.out && rm junk.out
+
+done
+
