e25d48b74fcea8a1280bccfb887eb77e05bab081
rotor <rotor>
2007-08-13 13:36:00 +0000

    * added -width and -vertical/-horizontal arguments to mincpik (thanks sebas)

--- autogen.sh
+++ autogen.sh
@@ -6,7 +6,7 @@ The required m4 files were not found.
 You need to check these out from their repository
 using
 
-    cvs -d /software/source checkout -d m4 libraries/mni-acmacros
+    cvs -d cvs.bic.mni.mcgil.ca:/private-cvsroot checkout -d m4 libraries/mni-acmacros
 
 (yes, two '-d' options)
 Then re-run autogen.sh.
@@ -14,18 +14,6 @@ EOF
     exit 1
 fi
 
-
-cat <<EOF
-Messages of the following type may be safely ignored.
-Any other diagnostics may be a sign of trouble.  
-Let us know if something goes wrong.
-
-    automake: configure.in: installing [...]
-    warning: AC_TRY_RUN called without default to allow cross compiling
-
-
-
-EOF
 
 aclocal -I m4
 autoheader
