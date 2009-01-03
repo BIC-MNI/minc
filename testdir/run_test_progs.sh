931564e7bb8c3a29f10c0a9c0f2d30adca78a2ad
stever <stever>
2009-01-03 21:52:41 +0000

    Fix test case: run with proper path, and with proper args for mincdiff.

--- testdir/run_test_progs.sh
+++ testdir/run_test_progs.sh
@@ -2,7 +2,11 @@
 
 set -e
 
+root=`pwd`/..
+progs=${root}/progs
+
+PATH=${root}:${progs}/mincdiff::${progs}/mincpik:${progs}/mincheader:${progs}/minchistory:${progs}/mincview:${PATH}
+export PATH
+
 mincheader icv.mnc > /dev/null
-mincdiff icv.mnc icv_dim.mnc > /dev/null
-
-
+mincdiff icv.mnc icv.mnc > /dev/null
