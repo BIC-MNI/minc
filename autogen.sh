2ed63f74681b497acee1eac2271509143b7853b1
Vladimir S. FONOV <vladimir.fonov@gmail.com>
2011-04-21 13:35:01 -0400

    Added files needed for autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -3,13 +3,6 @@
 if [ ! -r m4/mni_REQUIRE_LIB.m4 ]; then
     cat <<EOF
 The required m4 files were not found.
-You need to check these out from their repository
-using
-
-    cvs -d cvs.bic.mni.mcgil.ca:/private-cvsroot checkout -d m4 libraries/mni-acmacros
-
-(yes, two '-d' options)
-Then re-run autogen.sh.
 EOF
     exit 1
 fi
