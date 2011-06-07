acbabe274b2da8b1404c10bff7425ece224416bf
Leila Baghdadi <baghdadi@phenogenomics.ca>
2011-06-07 10:12:57 -0400

    to reflect voxel ordering properly

--- libsrc2/dimension.c
+++ libsrc2/dimension.c
@@ -1330,13 +1330,31 @@ miget_dimension_start(midimhandle_t dimension, mivoxel_order_t voxel_order,
   }
   
  
-  if (voxel_order == 0) {
+  if (voxel_order == MI_ORDER_FILE) {
     *start_ptr = dimension->start;
   }
-  else {
+ else { // L.B March 16/2011, Properly reflect voxel ordering
+    if (dimension->flipping_order == MI_COUNTER_FILE_ORDER) { 
+      *start_ptr = dimension->start + (dimension->step * (dimension->length-1));
+    }
+    else if (dimension->flipping_order == MI_POSITIVE) {
+      if (dimension->step > 0) {
+      *start_ptr = dimension->start;
+      }
+      else {
+	*start_ptr = dimension->start + (dimension->step * (dimension->length-1));
+      }
+    }
+    else if (dimension->flipping_order == MI_NEGATIVE) {
+      if (dimension->step < 0) {
+	*start_ptr = dimension->start;
+      }
+      else {
     
-    *start_ptr = dimension->start + (dimension->step * (dimension->length-1));
-  }
+	*start_ptr = dimension->start + (dimension->step * (dimension->length-1));
+      }
+    }
+ }
   return (MI_NOERROR);
 }
 
--- libsrc2/hyper.c
+++ libsrc2/hyper.c
@@ -744,7 +744,28 @@ miget_real_value_hyperslab(mihandle_t volume,
     miicv_setstr(icv, MI_ICV_SIGN, is_signed ? MI_SIGNED : MI_UNSIGNED);
     miicv_setint(icv, MI_ICV_DO_RANGE, TRUE);
     miicv_setint(icv, MI_ICV_DO_NORM, TRUE);
-
+    miicv_setint(icv, MI_ICV_DO_DIM_CONV, FALSE);
+    //figure out whether we need to flip image    L.B May 18/2011
+    for (i=0; i < ndims ; i++)
+      {
+	hdim = volume->dim_handles[i];
+	switch (hdim->flipping_order) {
+	case MI_FILE_ORDER:
+	  miicv_setint(icv, MI_ICV_DO_DIM_CONV, FALSE);
+	   break;
+	case MI_COUNTER_FILE_ORDER:
+	case MI_POSITIVE:
+	  if (hdim->step < 0)
+	    miicv_setint(icv, MI_ICV_DO_DIM_CONV, TRUE);
+	  break;
+	case MI_NEGATIVE: 
+	  if (hdim->step > 0)
+	    miicv_setint(icv, MI_ICV_DO_DIM_CONV, TRUE);
+	  break;
+	default:
+	  return;
+	}
+      }
     result = miicv_attach(icv, file_id, var_id);
     if (result == MI_NOERROR) {
 	result = mirw_hyperslab_icv(MIRW_OP_READ,
