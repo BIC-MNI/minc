1ea27f95f67353e914eee7926139953d945d699b
Vladimir S. FONOV <vladimir.fonov@gmail.com>
2012-05-24 15:18:10 -0400

    added generalized overlap measure example

--- ezminc/examples/volume_gtc_similarity.cpp
+++ ezminc/examples/volume_gtc_similarity.cpp
@@ -1,6 +1,6 @@
 /* ----------------------------- MNI Header -----------------------------------
-@NAME       :  volume_similarity
-@DESCRIPTION:  an example of calculating volume similarity metrics 
+@NAME       :  volume_gtc_similarity
+@DESCRIPTION:  an example of calculating generalized volume similarity metrics 
 @COPYRIGHT  :
               Copyright 2011 Vladimir Fonov, McConnell Brain Imaging Centre, 
               Montreal Neurological Institute, McGill University.
@@ -22,14 +22,14 @@ using namespace minc;
 
 void show_usage (const char * prog)
 {
-  std::cout<<"Program calculates volume similarity metrics"<<std::endl
-           <<"if multiple labels are present, similarity metrics will be calculated for each label separately"<<std::endl
-          <<"based on : M. Feuerman and A. R. Miller "
-          <<"\"Relationships between statistical measures of agreement: sensitivity, specificity and kappa\""
-          <<" Journal of Evaluation in Clinical Practice vol. 14 no. 5 pp 930-933 2008"<<std::endl
-          <<"http://dx.doi.org/10.1111/j.1365-2753.2008.00984.x"<<std::endl<<std::endl
-          <<"Usage: "<<prog<<" <input1.mnc> <input2.mnc> [--kappa --sensitivity --specificity --verbose --jaccard --csv]"<<std::endl;
   
+  std::cout<<"Program calculates multiple volume similarity metrics for discrete labels "<<std::endl
+           <<"or Generalized Tanimoto coefficient (GTC)" <<std::endl
+           <<"based on :  William R. Crum, Oscar Camara, and Derek L. G. Hill"
+           <<"\"Generalized Overlap Measures for Evaluation and Validation in Medical Image Analysis \""
+           <<" IEEE TRANSACTIONS ON MEDICAL IMAGING, VOL. 25, NO. 11, NOVEMBER 2006"<<std::endl
+           <<"http://dx.doi.org/10.1109/TMI.2006.880587"<<std::endl<<std::endl
+           <<"Usage: "<<prog<<" <input1.mnc> <input2.mnc> [--gkappa] [--gtc] [--csv]  "<<std::endl;
 }
 
 template<class T>class find_min_max
@@ -75,15 +75,13 @@ int main(int argc,char **argv)
 {
   int verbose=0;
   int csv=0;
-  int kappa=0,specificity=0,sensitivity=0;
-  int jaccard=0;
+  int gkappa=0;
+  int gtc=0;
   static struct option long_options[] = {
     {"verbose", no_argument,             &verbose, 1},
     {"quiet",   no_argument,             &verbose, 0},
-    {"kappa",   no_argument,             &kappa, 1},
-    {"sensitivity",   no_argument,       &sensitivity, 1},
-    {"specificity",   no_argument,       &specificity, 1},
-    {"jaccard",       no_argument,       &jaccard, 1},
+    {"gkappa",   no_argument,            &gkappa, 1},
+    {"gtc",      no_argument,       &gtc, 1},
     {"csv",           no_argument,       &csv, 1},
     {0, 0, 0, 0}
   };
@@ -117,13 +115,11 @@ int main(int argc,char **argv)
     return 1;
   }
   
-  if(! (sensitivity||specificity||kappa||jaccard) ) //no options selected, do all 
+  if(! (gkappa||gtc) ) //no options selected, do all 
   {
     verbose=1;
-    sensitivity=1;
-    specificity=1;
-    kappa=1;
-    jaccard=1;
+    gkappa=1;
+    gtc=1;
   }
   
   try
@@ -171,9 +167,6 @@ int main(int argc,char **argv)
     
     if( verbose && !csv)
     {
-      //std::cout<<buffer1.size() << std::endl;
-      //std::cout<<buffer1.begin()<<" "<<buffer1.end()<<std::endl;
-
       std::cout<<"Volume 1 min="<<(int)f1.min()<<" max="<<(int)f1.max()<<" count="<<f1.count()<<std::endl;
       std::cout<<"Volume 2 min="<<(int)f2.min()<<" max="<<(int)f2.max()<<" count="<<f2.count()<<std::endl;
     }
@@ -183,70 +176,40 @@ int main(int argc,char **argv)
 
     if(low==0) low=1; //assume 0 is background
 
-
+    double intersect=0.0;
+    double overlap=0.0;
+    double volume=0.0;
+    
+    //TODO: add mask ? or different weighting
     for(unsigned char label=low; label<=hi; label++)
     {
-      int v1=0,v2=0;
-      double a=0.0,b=0.0,c=0.0,d=0.0;
       for(int i=0; i<size ; i++ )
       {
-        if( buffer1[i]==label ) v1++;
-        if( buffer2[i]==label ) v2++;
-        if( buffer1[i]==label && buffer2[i]==label ) a++;
-        if( buffer1[i]==label && buffer2[i]!=label ) c++;
-        if( buffer1[i]!=label && buffer2[i]==label ) b++;
-        if( buffer1[i]!=label && buffer2[i]!=label ) d++;
+        if( buffer1[i]==label ) volume+=1.0;
+        if( buffer2[i]==label ) volume+=1.0;
+        
+        if( buffer1[i]==label && buffer2[i]==label ) intersect+=1.0;
+        if( buffer1[i]==label || buffer2[i]==label ) overlap+=1.0;
       }
-      double _kappa=0.0;
-      double _sensitivity=0.0;
-      double _specificity=0.0;
-      double _jaccard=0.0;
+    }
+    double _gkappa=2*intersect/volume;
+    double _gtc=intersect/overlap;
+    
+    std::cout.precision(10);
 
-      if(v1>0 && v2>0)
-      {
-        //_kappa=v1v2*2.0/(v1+v2);
-        _kappa=a*2.0/(v1+v2);
-
-        _sensitivity=a/(a+c);
-        _specificity=d/(b+d);
-
-        _jaccard=a/(a+c+b);
+    if( csv )
+    {
+      std::cout<<_gkappa<<",";
+      std::cout<<_gtc<<std::endl;
+    } else {
+      if( gkappa ){
+        if(verbose) std::cout<<"Generalized Kappa ";
+        std::cout<<_gkappa<<std::endl;
       }
-
-      std::cout.precision(10);
-
-      if( hi!=low && verbose && !csv)
-        std::cout<<"Label: "<<(int)label<<std::endl;
-
-      if( csv )
+      if( gtc )
       {
-        if(low!=hi)
-          std::cout<<(int)label<<",";
-
-        std::cout<<_kappa<<",";
-        std::cout<<_sensitivity<<",";
-        std::cout<<_specificity<<",";
-        std::cout<<_jaccard<<std::endl;
-      } else {
-        if( kappa ){
-          if(verbose) std::cout<<"Kappa ";
-          std::cout<<_kappa<<std::endl;
-        }
-        if( sensitivity )
-        {
-          if(verbose) std::cout<<"Sensitivity ";
-          std::cout<<_sensitivity<<std::endl;
-        }
-        if( specificity )
-        {
-          if(verbose) std::cout<<"Specificity ";
-          std::cout<<_specificity<<std::endl;
-        }
-        if( jaccard )
-        {
-          if(verbose) std::cout<<"Jaccard similarity ";
-          std::cout<<_jaccard<<std::endl;
-        }
+        if(verbose) std::cout<<"Generalized Tinamoto Coeffecient ";
+        std::cout<<_gtc<<std::endl;
       }
     }
     
